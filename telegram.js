64bdb4ba80fc8d3d563333f2d6d8db838df1cad5
Rezis Github <cosmonshop@gmail.com>
2026-02-15 18:45:31 +0400

    Update telegram.js

--- telegram.js
+++ telegram.js
@@ -1,14 +1,10 @@
 import axios from "axios";
-import dotenv from "dotenv";
-dotenv.config();
 
-export async function sendTelegram(message) {
-  await axios.post(
-    `https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/sendMessage`,
-    {
-      chat_id: process.env.TELEGRAM_CHAT_ID,
-      text: message,
-      disable_web_page_preview: true
-    }
-  );
+export async function sendToTelegram(message) {
+  const url = `https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/sendMessage`;
+
+  await axios.post(url, {
+    chat_id: process.env.TELEGRAM_CHAT_ID,
+    text: message
+  });
 }
